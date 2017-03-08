029ee1c6d94029a46e67b0496f9f2ae1079b121a
Cody Higgins <cody.higginsdev18@gmail.com>
2017-03-08 09:29:56 -0700

    added a code snipet to test.rb

--- test.rb
+++ test.rb
@@ -1,19 +1,4 @@
-teams = {
-    "Houston Astros" => {
-      "first base" => "Chris Carter",
-      "second base" => "Jose Altuve",
-      "shortstop" => "Carlos Correa"
-    },
-    "Texas Rangers" => {
-      "first base" => "Prince Fielder",
-      "second base" => "R. Odor",
-      "shortstop" => "Elvis Andrus"
-    }
-} 
-
-teams.each do |team, players|
-  puts team
-  players.each do |position, player|
-    p "#{player} starts at #{position}"
+  def word_list
+    Hash[%w(the quick brown fox / jumped over the lazy fox.).length { |x| [x,x.count]}]
+      p x
   end
-end
