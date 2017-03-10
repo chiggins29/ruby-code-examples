ac7d38e20491b222ff1b5e7c5ab5755161a2ef5d
Cody Higgins <cody.higginsdev18@gmail.com>
2017-03-10 10:31:09 -0700

    added to test file

--- test.rb
+++ test.rb
@@ -1,26 +1,28 @@
 drinks = {
-		drpepper: "1.99",
-		pepsi: "1.89",
-		lemonaid: "1.50"
+		"drpepper" => "1.99",
+		"pepsi" => "1.89",
+		"lemonaid" => "1.50"
 	}
-			
+		puts drinks	
+		puts "What drink would you like?"
+		pick = gets.chomp
 
-			puts "What drink would you like?"
-
-			pick = gets.chomp
-
-			pick = Hash.new{}
       
-        drinks.each do |key, value|
-          if drinks.include? pick.to_sym
-            puts "#{pick}, #{value}"
-          else
-            puts "Error, item not found!"
-          end
-        end
+      	if pick == "drpepper" do drinks.fetch("drpepper"){|k|}
+      		puts "You bought drpepper for #{k}"
+      	elsif pick == "pepsi"
+      		puts "You bought pepsi"
+      	elsif pick == "lemonaid"
+      		puts "You bought lemonaid" 
+      	end 
 
 
 
+empty = Hash.new
+empty[pick.to_sym]
 
 
+#this will print just the value of drpepper
+drinks.fetch("drpepper"){|k| puts k}
 
+drinks.fetch(pick){|k| puts k}
