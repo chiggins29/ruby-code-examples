6c1998e22f9c5ef8d3400fc8cd554882d174b747
Cody Higgins <cody.higginsdev18@gmail.com>
2017-03-20 09:56:08 -0600

    daily challenge added

---
+++ daily_challenges/bob.rb
@@ -0,0 +1,13 @@
+puts "Say something to bob"
+input = gets.chomp
+
+if input.downcase.empty?
+  puts "fine be that way"
+elsif input == input.upcase!
+  puts "Whoa chill out"
+elsif input.split(//).last == '?'
+  puts "sure"
+else
+  puts "whatever"
+end
+
