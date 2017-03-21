c162e5132b35fb77bfc209c3915255b6a2b4122e
Cody Higgins <cody.higginsdev18@gmail.com>
2017-03-21 13:30:11 -0600

    updated files for daily challenge

--- daily_challenges/bob.rb
+++ daily_challenges/bob.rb
@@ -3,7 +3,7 @@ input = gets.chomp
 
 if input.downcase.empty?
   puts "fine be that way"
-elsif input == input.upcase!
+elsif input == input.upcase
   puts "Whoa chill out"
 elsif input.split(//).last == '?'
   puts "sure"
