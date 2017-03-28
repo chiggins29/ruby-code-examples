e64c2919c94f3800c608d09342b8ee63b79ebe4f
Cody Higgins <cody.higginsdev18@gmail.com>
2017-03-28 11:08:26 -0600

    made some small changes to guessing game

--- daily_challenges/binary_guessing_game.rb
+++ daily_challenges/binary_guessing_game.rb
@@ -4,10 +4,8 @@ puts "User, pick a number from 1-25"
 user_input = gets.chomp.to_i
 
 until cpu_pick == user_input
+	 print "You win"
 if
-  cpu_pick == user_input
-    print "You win"
-elsif 
   user_input > cpu_pick
     puts "Your pick is higher, pick again"
     user_input = gets.chomp.to_i
