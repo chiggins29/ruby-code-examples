f659d2c2caed58e418b8d86e42942e38aaccb2e0
Cody Higgins <cody.higginsdev18@gmail.com>
2017-04-11 17:10:26 -0600

    added a new file and updated original file with types

--- Type-script/hello_typescript.js
+++ Type-script/hello_typescript.js
@@ -4,28 +4,8 @@ function hey_there() {
 hey_there();
 //# sourceMappingURL=hello_typescript.js.map
 
-// Types in typescript
 
-//string
-var fullName : string = "Cody Higgins";
-// boolean
-let paidAccount : boolean = true;
-// number
-const versionNumber : number = 1.3;
-// array
-var ages : number[] = [33, 54, 67, 12];
-// tuple
-let player : [number, string, number, boolean];
-player = [23, "Favorite Number", 24, true];
-// eNum
-enum ApprovalStatus {Approved, Pending, Rejected};
-let job ApprovalStatus = ApprovalStatus.Pending;
-// any
-var apidata : any[] = [123, "Cody", false];
-// void
-function printOut(msg: string) : void {
-	console.log(msg);
-}
+
 
 
 
--- Type-script/hello_typescript.ts
+++ Type-script/hello_typescript.ts
@@ -2,3 +2,26 @@ function hey_there() {
 	console.log("Hi from typescript");
 }
 hey_there();
+
+
+//string
+var fullName : string = "Cody Higgins";
+// boolean
+let paidAccount : boolean = true;
+// number
+const versionNumber : number = 1.3;
+// array
+var ages : number[] = [33, 54, 67, 12];
+// tuple
+let player : [number, string, number, boolean];
+player = [23, "Favorite Number", 24, true];
+// eNum
+enum ApprovalStatus {Approved, Pending, Rejected};
+let job : ApprovalStatus = ApprovalStatus.Pending;
+// any use when there isnt any other option
+var apidata : any[] = [123, "Cody", false];
+// void method that performs actions but
+// shouldn't return a value back
+function printOut(msg: string) : void {
+	console.log(msg);
+}
